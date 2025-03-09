65e955a7a76c237674b9a7234cab567b9e942272
Jared Grove <jgrove@shopmonkey.io>
2025-03-09 13:58:47 -0500

    idk this is what mailchimp does

--- src/components/TableWrapper.tsx
+++ src/components/TableWrapper.tsx
@@ -6,26 +6,9 @@ interface SelectableProps {
   textAlign: "center" | "left" | "right";
 }
 const TableWrapper = ({ children, textAlign }: SelectableProps) => {
-  const isReactElement = React.isValidElement(children);
-
   return (
     <Row>
-      <Column align={textAlign} style={{ textAlign }}>
-        {isReactElement
-          ? React.cloneElement(children, {
-              style: {
-                ...children.props.style,
-                display: "block",
-                margin:
-                  textAlign === "center"
-                    ? "0 auto"
-                    : textAlign === "right"
-                    ? "0 0 0 auto"
-                    : undefined,
-              },
-            })
-          : children}
-      </Column>
+      <Column align={textAlign}>{children}</Column>
     </Row>
   );
 };
